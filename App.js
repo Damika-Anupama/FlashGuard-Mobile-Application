dee298876faef07f0d0dfce5d67bd507cbee1062
Damika <damikaanupama@gmail.com>
2023-01-01 04:05:36 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignUp from "./pages/SignUp"; // import the SignUp component
+import SignIn from "./pages/SignIn"; // import the SignIn component
+import Dashboard from "./pages/Dashboard"; // import the SignIn component
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen name="Sign Up" component={SignUp} />
+          <Stack.Screen name="Sign In" component={SignIn} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+}
+
+const Welcome= props => {
+  return (
+    <LinearGradient colors={['#1E90FF', '#00008B']} style={styles.gradient}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Welcome to FlashGuard App</Text>
+      <Image source={require("./assets/flashguard.jpg")} style={styles.image} />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Sign Up"
+          onPress={() => props.navigation.navigate("Sign Up")}
+        />
+        <Button
+          title="Sign In"
+          onPress={() => props.navigation.navigate("Sign In")}
+        />
+      </View>
+      <StatusBar style="light" />
     </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "white",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    marginVertical: 16,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginVertical: 8,
+    margin: 8,
   },
 });
+
+export default App;
---
+++ pages/Dashboard.js
@@ -0,0 +1,214 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text, Image, Button, ScrollView, Table, TableRow, TableCell } from 'react-native';
+
+export function Dashboard() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.upperNav}>
+        <Image
+          source={require('../assets/notification.png')}
+          style={styles.notificationIcon}
+        />
+        <Image
+          source={require('../assets/exit.png')}
+          style={styles.exitIcon}
+        />
+      </View>
+      <ScrollView style={styles.scrollPane}>
+        <View style={styles.mainContent}>
+          <View style={styles.connectArea}>
+            <Text style={styles.connectTitle}>FlashGuard Connect</Text>
+            <Button
+              title="Connect Device"
+              icon={
+                <Image
+                  source={require('../assets/bluetooth.png')}
+                  style={styles.bluetoothIcon}
+                />
+              }
+              onPress={() => {}}
+            />
+            <View style={styles.deviceStatus}>
+              <Text style={styles.deviceStatusText}>Device Status: </Text>
+              <Image
+                source={require('../assets/connected.png')}
+                style={styles.statusIcon}
+              />
+            </View>
+          </View>
+          <View style={styles.stimulusDetails}>
+            <Text style={styles.stimulusDetailsTitle}>Your Previous Stimulus Details</Text>
+            <Table style={styles.stimulusTable}>
+              <TableRow>
+                <TableCell style={styles.tableHeader}>Type</TableCell>
+                <TableCell style={styles.tableHeader}>Frequency</TableCell>
+                <TableCell style={styles.tableHeader}>Duration</TableCell>
+                <TableCell style={styles.tableHeader}>Date and Time</TableCell>
+                <TableCell style={styles.tableHeader}>Intensity</TableCell>
+                <TableCell style={styles.tableHeader}>Location</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={styles.tableCell}>Vibration</TableCell>
+                <TableCell style={styles.tableCell}>5 Hz</TableCell>
+                <TableCell style={styles.tableCell}>30 min</TableCell>
+                <TableCell style={styles.tableCell}>Jan 1, 2021 10:00 AM</TableCell>
+                <TableCell style={styles.tableCell}>50%</TableCell>
+                <TableCell style={styles.tableCell}>Left wrist</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={styles.tableCell}>Sound</TableCell>
+                <TableCell style={styles.tableCell}>10 Hz</TableCell>
+                <TableCell style={styles.tableCell}>15 min</TableCell>
+                <TableCell style={styles.tableCell}>Jan 2, 2021 11:00 AM</TableCell>
+                <TableCell style={styles.tableCell}>70%</TableCell>
+                <TableCell style={styles.tableCell}>Right ear</TableCell>
+              </TableRow>
+            </Table>
+          </View>
+        </View>
+      </ScrollView>
+      <View style={styles.footer}>
+        <Button
+          title="Home"
+          icon={
+            <Image
+              source={require('../assets/home.png')}
+              style={styles.footerIcon}
+            />
+          }
+          onPress={() => {}}
+          style={styles.footerButton}
+        />
+        <Button
+          title="Settings"
+          icon={
+            <Image
+              source={require('../assets/settings.png')}
+              style={styles.footerIcon}
+            />
+          }
+          onPress={() => {}}
+          style={styles.footerButton}
+        />
+        <Button
+          title="Feedback"
+          icon={
+            <Image
+              source={require('../assets/feedback.png')}
+              style={styles.footerIcon}
+            />
+          }
+          onPress={() => {}}
+          style={styles.footerButton}
+        />
+        <Button
+          title="Additional Details"
+          icon={
+            <Image
+              source={require('../assets/details.png')}
+              style={styles.footerIcon}
+            />
+          }
+          onPress={() =>{}}
+          style={styles.footerButton}
+        />
+      </View>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  upperNav: {
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingHorizontal: 16,
+  },
+  notificationIcon: {
+    width: 24,
+    height: 24,
+  },
+  exitIcon: {
+    width: 24,
+    height: 24,
+    marginLeft: 16,
+  },
+  scrollPane: {
+    flex: 1,
+    backgroundColor: '#00008B',
+  },
+  mainContent: {
+    padding: 32,
+  },
+  connectArea: {
+    marginBottom: 32,
+  },
+  connectTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 16,
+  },
+  bluetoothIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 8,
+  },
+  deviceStatus: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  deviceStatusText: {
+    fontSize: 16,
+    color: '#fff',
+  },
+  statusIcon: {
+    width: 24,
+    height: 24,
+    marginLeft: 8,
+  },
+  stimulusDetails: {
+    marginTop: 32,
+  },
+  stimulusDetailsTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 16,
+  },
+  stimulusTable: {
+    width: '100%',
+  },
+  tableHeader: {
+    backgroundColor: '#add8e6',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#00008B',
+  },
+  tableCell: {
+    fontSize: 16,
+    color: '#fff',
+  },
+  footer: {
+    height: 60,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: '#fff',
+  },
+  footerButton: {
+    flex: 1,
+  },
+  footerIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 8,
+  },
+});
