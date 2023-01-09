af0eea9bce286d006bdaec6bd89d46ec3d7e2179
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2023-01-09 09:27:49 +0530

    Remove unused imports

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { Button } from 'react-native-paper';
 import { Welcome } from './screens/Welcome';
 import React from 'react';
 
