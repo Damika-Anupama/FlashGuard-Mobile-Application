e2c519ec09630cf2348bca72727e4e7d04207b40
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2023-01-09 10:03:47 +0530

    Fix imports

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Provider as PaperProvider } from 'react-native-paper'
-import { Welcome } from './screens/Welcome'
+import Welcome from './screens/Welcome'
 
 const Stack = createStackNavigator()
 
