f8174e3ff4ed2825fcfe62e60a22f7dffcf724c0
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2023-01-09 12:14:29 +0530

    Add tab navigation

--- App.jsx
+++ App.jsx
@@ -1,24 +1,47 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Provider as PaperProvider } from 'react-native-paper'
-import Welcome from './src/screens/Welcome'
-import SignUp from './src/screens/SignUp'
-import SignIn from './src/screens/SignIn'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import Home from './src/screens/Home'
 import Dashboard from './src/screens/Dashboard'
+import Device from './src/screens/Device'
+import Profile from './src/screens/Profile'
 
-const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 function App() {
+  const tabBarIcon = ({ focused, color, size }, route) => {
+    let iconName
+
+    if (route.name === 'Home') {
+      iconName = focused ? 'home' : 'home-outline'
+    } else if (route.name === 'Dashboard') {
+      iconName = focused ? 'bar-chart' : 'bar-chart-outline'
+    } else if (route.name === 'Device') {
+      iconName = focused ? 'glasses' : 'glasses-outline'
+    } else if (route.name === 'Profile') {
+      iconName = focused ? 'person' : 'person-outline'
+    }
+
+    // You can return any component that you like here!
+    return <Ionicons name={iconName} size={size} color={color} />
+  }
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Welcome" component={Welcome} />
-          <Stack.Screen name="Sign Up" component={SignUp} />
-          <Stack.Screen name="Sign In" component={SignIn} />
-          <Stack.Screen name="Dashboard" component={Dashboard} />
-        </Stack.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: (props) => tabBarIcon(props, route),
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Dashboard" component={Dashboard} />
+          <Tab.Screen name="Device" component={Device} />
+          <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
       </NavigationContainer>
     </PaperProvider>
   )
--- src/screens/Device.jsx
+++ src/screens/Device.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-export default function Home() {
+export default function Device() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Device</Text>
     </View>
   )
 }
