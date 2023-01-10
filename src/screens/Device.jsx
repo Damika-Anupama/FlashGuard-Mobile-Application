661617c00eb07e7bf26b707779e04f49af0ed93b
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2023-01-10 16:28:13 +0530

    Add UI for connecting device

--- src/screens/Device.jsx
+++ src/screens/Device.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
+import { Button } from 'react-native-paper'
 
 export default function Device() {
+  const handleConnectDevice = () => {
+    console.log('Connecting Device...')
+  }
+
   return (
-    <View>
-      <Text>Device</Text>
-    </View>
+    <ScrollView className="bg-white">
+      <View className="flex items-center justify-center p-4 m-4 space-y-3 bg-white rounded-md shadow shadow-black">
+        <Text className="text-lg text-gray-600">Device not connected</Text>
+        <Button mode="outlined" onPress={() => handleConnectDevice}>
+          Connect Device
+        </Button>
+      </View>
+    </ScrollView>
   )
 }
