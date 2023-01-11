ef8939096d2c774dd0ac607fd7a8a1601dbb9fe5
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2023-01-11 11:17:38 +0530

    Add profile section

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import React, { useCallback } from 'react'
+import { ScrollView, Text, View } from 'react-native'
+import { Avatar, Card, List } from 'react-native-paper'
 
 export default function Profile() {
+  const ListIconRight = useCallback(
+    (props) => <List.Icon {...props} icon="chevron-right" />,
+    []
+  )
+  const ListIconLogOut = useCallback(
+    (props) => <List.Icon {...props} icon="logout" />,
+    []
+  )
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <ScrollView className="mb-2 bg-gray-50">
+      <Card className="m-4 bg-white">
+        <Card.Content className="flex items-center justify-center p-4">
+          <Avatar.Image
+            size={100}
+            source={require('../../assets/profile.jpg')}
+            className="mb-2"
+          />
+          <Text className="text-2xl font-bold text-gray-700">
+            John Roberson
+          </Text>
+          <Text className="text-lg font-bold text-gray-500">California</Text>
+        </Card.Content>
+      </Card>
+
+      <View>
+        <Card className="m-4 bg-white">
+          <Card.Content className="p-2">
+            <List.Item title="Account" description="" right={ListIconRight} />
+            <List.Item title="Settings" description="" right={ListIconRight} />
+            <List.Item title="Help" description="" right={ListIconRight} />
+            <List.Item title="Logout" description="" right={ListIconLogOut} />
+          </Card.Content>
+        </Card>
+      </View>
+    </ScrollView>
   )
 }
