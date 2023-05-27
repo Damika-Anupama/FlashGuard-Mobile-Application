08f677da08874c606d1e834afa7a4aff9b0d4e1a
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2023-05-27 09:53:46 +0530

    fix graph styling

--- src/components/RealTimeGraph.jsx
+++ src/components/RealTimeGraph.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { View, Dimensions } from 'react-native'
-import { Svg, Path } from 'react-native-svg'
+import { View, Dimensions, Text } from 'react-native'
+import { Svg, Path, Line } from 'react-native-svg'
 import PropTypes from 'prop-types'
 import useHazardData from '../hooks/hazardData'
 
@@ -8,10 +8,52 @@ const windowSize = 30
 const verticalPadding = 40
 const horizontalPadding = 40
 const height = 200
-const { width } = Dimensions.get('window')
+const width = Dimensions.get('window').width - 50
 
-const scaleY = (value, h) => h - (value / 100) * h
-const scaleX = (value, w) => (value / windowSize) * (w - horizontalPadding)
+const scaleY = (value, h) => h - (value / 50) * h
+const scaleX = (value, w) =>
+  (value / (windowSize - 2)) * (w - horizontalPadding)
+
+function YAxisLabels() {
+  const yAxisHeight = height - verticalPadding
+  const numberOfLabels = 6
+  const labels = Array.from(
+    { length: numberOfLabels },
+    (_, i) => ((numberOfLabels - i - 1) / (numberOfLabels - 1)) * 50
+  )
+
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        left: 0,
+        height: yAxisHeight,
+        justifyContent: 'space-between',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+      }}
+    >
+      {labels.map((label, i) => (
+        <View key={i}>
+          <Text className="text-xs text-gray-500">{label}</Text>
+        </View>
+      ))}
+    </View>
+  )
+}
+
+function YAxis() {
+  return (
+    <Line
+      x1="0"
+      y1="0"
+      x2="0"
+      y2={height - verticalPadding}
+      stroke="#5482D6"
+      strokeWidth="4"
+    />
+  )
+}
 
 function GraphPath({ graphData }) {
   const pathData = graphData
@@ -24,7 +66,7 @@ function GraphPath({ graphData }) {
     )
     .join(' ')
 
-  return <Path d={pathData} fill="none" stroke="#2370FF" strokeWidth="2" />
+  return <Path d={pathData} fill="none" stroke="#5482D6" strokeWidth="2" />
 }
 
 function RealTimeGraph() {
@@ -48,17 +90,24 @@ function RealTimeGraph() {
   // Add flash data to the graph when it's available
   useEffect(() => {
     if (flashData) {
+      const [firstNumber] = flashData.split(',')
       setGraphData((prevData) => {
         const newData = [...prevData]
-        newData[newData.length - 1] = 100
+        newData[newData.length - 1] = Number(firstNumber) || 0
         return newData
       })
     }
   }, [flashData])
 
   return (
-    <View>
-      <Svg height={height} width={width - horizontalPadding}>
+    <View style={{ width, height, marginTop: 20 }}>
+      <YAxisLabels />
+      <Svg
+        height={height}
+        width={width - horizontalPadding}
+        className="absolute top-0 right-0"
+      >
+        <YAxis />
         <GraphPath graphData={graphData} />
       </Svg>
     </View>
